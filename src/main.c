250510b8c97d2c5c061f1dac9b2da1a8aa714a04
Tyler Evans <evstye@gmail.com>
2025-01-20 13:10:22 -0700

    setup the cmake file working with the compile commands not complete just yet

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "FreeRTOS.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
 
 int main(){
+    //stdio_init_all();
     printf("hello world\n");
+    while(1){
+        stdio_init_all();
+
+    }
     return 0;
 }
