62e76a618cd9d8e9caff45643b36af9c48566d97
Tyler Evans <evstye@gmail.com>
2025-01-20 13:33:46 -0700

    working files without README complete

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <hardware/gpio.h>
+#include <pico/time.h>
 #include <stdio.h>
 #include "FreeRTOS.h"
 #include "pico/stdlib.h"
@@ -5,11 +7,17 @@
 #include "pico/cyw43_arch.h"
 
 int main(){
-    //stdio_init_all();
+    //this inits the stdio
+    stdio_init_all();
     printf("hello world\n");
-    while(1){
-        stdio_init_all();
+    gpio_init(CYW43_WL_GPIO_LED_PIN);
 
+
+    while(1){
+        gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(100);
     }
     return 0;
 }
