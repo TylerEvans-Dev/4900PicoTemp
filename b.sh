b2f3878212222c975959e7e83562f734633c1854
Tyler Evans <evstye@gmail.com>
2025-02-03 09:11:06 -0700

    working with the bash script

--- b.sh
+++ b.sh
@@ -3,5 +3,23 @@ set -e #exits upon a bad comand
 
 cmake -B build -S .
 cd build
-make 
+make
 echo "Generated files complete"
+cd ..
+
+#This is the copy and paste command of the uf2 file
+ls
+echo "the file path is above"
+
+UF2_FILE="build/robo.uf2"
+
+P_LOC="~../../mnt/e/"
+
+if [ -d "$P_LOC" ]; then
+	echo "pico is detected at $P_LOC"
+
+else
+	echo "the pico is not present"
+	exit 1
+
+fi
